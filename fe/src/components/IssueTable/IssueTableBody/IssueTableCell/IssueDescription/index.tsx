4cd16316169a609d4e7d1cea30c5f74cabb4af84
happyGyu <happyboyto.dev@gmail.com>
2022-06-20 16:55:50 +0900

    feat: 이슈 설명 기능 추가

--- fe/src/components/IssueTable/IssueTableBody/IssueTableCell/IssueDescription/index.tsx
+++ fe/src/components/IssueTable/IssueTableBody/IssueTableCell/IssueDescription/index.tsx
@@ -1,25 +1,42 @@
+import SignpostOutlinedIcon from '@mui/icons-material/SignpostOutlined';
 import styled from 'styled-components';
 
 import IconTextBox from '@components/IconTextBox';
 import colors from '@constants/colors';
 import mixin from '@style/mixin';
+import { calTimePassed } from '@util/dateHandler';
 
 interface IIssueDescriptionProps {
   issueNum: number;
   writer: string;
   createdDatetime: string;
+  milestone: {
+    id: number;
+    name: string;
+  };
 }
 
 export default function IssueDescription({
   issueNum,
   writer,
   createdDatetime,
+  milestone,
 }: IIssueDescriptionProps) {
+  const writerAndTimeDescription = `이 이슈가 ${calTimePassed(
+    new Date(),
+    new Date(createdDatetime),
+  )} 전, ${writer}님에 의해 작성되었습니다.`;
+
   return (
     <Wrapper>
       <IconTextBox color={colors.label} texts={[`#${issueNum}`]} />
-      <IconTextBox color={colors.label} texts={[writer]} />
-      <IconTextBox color={colors.label} texts={[createdDatetime]} />
+      <IconTextBox color={colors.label} texts={[writerAndTimeDescription]} />
+      <IconTextBox
+        Icon={<SignpostOutlinedIcon fontSize="small" />}
+        color={colors.label}
+        texts={[milestone.name]}
+        spacing={0.5}
+      />
     </Wrapper>
   );
 }
