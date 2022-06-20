b8a2a29a92741c233516118e5ed37a5f2ab2e2d2
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-06-21 02:33:16 +0900

    feat: assignees layout

---
+++ fe/src/components/SideMenu/Labels/index.tsx
@@ -0,0 +1,9 @@
+import Container from '@components/Container';
+
+export default function Labels() {
+  return (
+    <Container flexInfo={{ direction: 'column' }} gap={1}>
+      <div>안녕 난 라벨</div>
+    </Container>
+  );
+}
--- fe/src/components/SideMenu/SideMenuItem.tsx
+++ fe/src/components/SideMenu/SideMenuItem.tsx
@@ -5,6 +5,9 @@ import styled from 'styled-components';
 import Container from '@components/Container';
 import DropdownModal from '@components/FilterDropdown/DropdownModal';
 //  dropDownInfo는 임시. 나중에는 API서버에서 정보를 받아와 구성해야 함.
+import Assignees from '@components/SideMenu/Assignees';
+import Labels from '@components/SideMenu/Labels';
+import Milestones from '@components/SideMenu/Milestones';
 import dropDownInfo from '@constants/temp';
 
 type ModalTypes = 'ASSIGNEE' | 'LABEL' | 'MILESTONE';
@@ -14,6 +17,19 @@ enum KoType {
   LABEL = '레이블',
   MILESTONE = '마일스톤',
 }
+
+const getSubBoxByType = (type) => {
+  switch (type) {
+    case 'ASSIGNEE':
+      return <Assignees />;
+    case 'LABEL':
+      return <Labels />;
+    case 'MILESTONE':
+      return <Milestones />;
+    default:
+      throw Error("Side Menu item's subbox is something wrong");
+  }
+};
 
 export default function SideMenuItem({
   type,
@@ -25,23 +41,23 @@ export default function SideMenuItem({
   onClickMenuItem: () => void;
 }> {
   return (
-    <ClickableContainer
-      position="relative"
-      flexInfo={{ align: 'center', justify: 'space-between' }}
-      padding="3rem 2rem"
-      onClick={() => onClickMenuItem(type)}
-    >
-      <TypeTitle>{KoType[type]}</TypeTitle>
-      <AddIcon />
-      {opendModalType === type && (
-        <DropdownModal left={4} top={5} info={dropDownInfo[type]} />
-      )}
-    </ClickableContainer>
+    <Container padding="2.5rem 2rem">
+      <Container
+        position="relative"
+        flexInfo={{ align: 'center', justify: 'space-between' }}
+        mb="1rem"
+      >
+        <TypeTitle>{KoType[type]}</TypeTitle>
+        <AddIcon
+          onClick={() => onClickMenuItem(type)}
+          sx={{ cursor: 'pointer' }}
+        />
+        {opendModalType === type && (
+          <DropdownModal left={4} top={5} info={dropDownInfo[type]} />
+        )}
+      </Container>
+      {getSubBoxByType(type)}
+    </Container>
   );
 }
-
-const ClickableContainer = styled(Container)`
-  cursor: pointer;
-`;
-
 const TypeTitle = styled.h6``;
