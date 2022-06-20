0121ef1cff3993830bfad7c722fae4f805cfe8f8
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-06-21 02:40:07 +0900

    feat: labels layout

--- fe/src/components/SideMenu/Labels/index.tsx
+++ fe/src/components/SideMenu/Labels/index.tsx
@@ -1,9 +1,18 @@
 import Container from '@components/Container';
+import Label from '@components/Label';
 
 export default function Labels() {
   return (
-    <Container flexInfo={{ direction: 'column' }} gap={1}>
-      <div>안녕 난 라벨</div>
+    <Container flexInfo={{ wrap: 'wrap' }} gap={0.5}>
+      <Label
+        key={12312}
+        text="프을리이즈으"
+        color="white"
+        bgColor="lightcoral"
+      />
+      <Label key={12312} text="기입스" color="white" bgColor="red" />
+      <Label key={1124} text="미히잉" color="white" bgColor="blue" />
+      <Label key={123124} text="오잉쥐" color="white" bgColor="orange" />
     </Container>
   );
 }
