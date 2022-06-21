996293101a91e71a5fab56782728e1466ba4d519
happyGyu <happyboyto.dev@gmail.com>
2022-06-22 01:48:01 +0900

    feat: patch issue handler

--- fe/src/server/handlers.ts
+++ fe/src/server/handlers.ts
@@ -14,7 +14,7 @@ interface IUser {
 
 let lastUserId = 1;
 const fakeUsers: IUser[] = [];
-const fakeIssues: IssueType[] = [...issues];
+let fakeIssues: IssueType[] = [...issues];
 
 const postJoin: Parameters<typeof rest.get>[1] = async (req, res, ctx) => {
   const { email, name, password } = req.body;
@@ -80,11 +80,25 @@ const postCreateIssue: Parameters<typeof rest.get>[1] = (req, res, ctx) => {
   return res(ctx.status(201));
 };
 
+const patchUpdatedStatus: Parameters<typeof rest.get>[1] = (req, res, ctx) => {
+  const { updatedStatus, idOfIssues } = req.body;
+  const updatedIssues = fakeIssues.map((issue) => {
+    const updatedIssue = { ...issue };
+    if (idOfIssues.includes(issue.id)) {
+      updatedIssue.status = updatedStatus;
+    }
+    return updatedIssue;
+  });
+  fakeIssues = updatedIssues;
+  return res(ctx.status(200), ctx.json(fakeIssues));
+};
+
 export default function handlers() {
   return [
     rest.post('/api/join', postJoin),
     rest.post('/api/login', postLogin),
     rest.get('/api/issues', getIssues),
     rest.post('/api/createIssue', postCreateIssue),
+    rest.patch('/api/issues/status/update', patchUpdatedStatus),
   ];
 }
