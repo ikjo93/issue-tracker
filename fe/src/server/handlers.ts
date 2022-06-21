391b722b89de16be0f3b9e457b248c38877a9fb9
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-06-22 01:41:22 +0900

    feat: label, member, milestone get api mock

--- fe/src/server/handlers.ts
+++ fe/src/server/handlers.ts
@@ -1,6 +1,15 @@
 import issueHandlers from '@server/handlers/issueHandlers';
+import labelHandlers from '@server/handlers/labelHandlers';
 import loginHandlers from '@server/handlers/loginHandlers';
+import memberHandlers from '@server/handlers/memberHandlers';
+import milestoneHandlers from '@server/handlers/milestoneHandlers';
 
 export default function handlers() {
-  return [...loginHandlers(), ...issueHandlers()];
+  return [
+    ...loginHandlers(),
+    ...issueHandlers(),
+    ...milestoneHandlers(),
+    ...memberHandlers(),
+    ...labelHandlers(),
+  ];
 }
