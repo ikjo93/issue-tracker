59d749550502356bb71a14efe734a105a5063ab2
ikjo93 <auddlr100@naver.com>
2022-06-26 03:41:53 +0900

    refactor : 깃허브 OAuth 관련 로직 RestTemplate -> WebClient 변경

--- be/src/main/java/codesquad/issuetracker/client/GithubOAuthClient.java
+++ be/src/main/java/codesquad/issuetracker/client/GithubOAuthClient.java
@@ -2,63 +2,51 @@ package codesquad.issuetracker.client;
 
 import codesquad.issuetracker.dto.auth.AuthMemberInformation;
 import codesquad.issuetracker.dto.auth.OAuthAccessToken;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class GithubOAuthClient {
 
     private static final String GITHUB_AUTHORIZATION_SERVER_URL = "https://github.com/login/oauth/access_token";
-    private static final String GITHUB_API_ACCEPT_HEADER = "application/vnd.github.v3+json";
     private static final String USER_SCOPE = "user";
     private static final String GITHUB_RESOURCE_SERVER_USER_API_URL = "https://api.github.com/" + USER_SCOPE;
 
     public OAuthAccessToken getAccessToken(String code) {
-        MultiValueMap<String, String> requestHeader = getRequestHeaderForAuth();
-        MultiValueMap<String, String> requestPayload = getRequestPayloadForAuth(code);
-        HttpEntity<?> request = new HttpEntity<>(requestPayload, requestHeader);
-
-        ResponseEntity<?> response = new RestTemplate().postForEntity(
-            GITHUB_AUTHORIZATION_SERVER_URL, request, OAuthAccessToken.class);
-
-        return (OAuthAccessToken) response.getBody();
+        return WebClient.create()
+            .post()
+            .uri(GITHUB_AUTHORIZATION_SERVER_URL)
+            .headers(header -> {
+                header.setBasicAuth(System.getenv("CLIENT_ID"), System.getenv("CLIENT_SECRET"));
+                header.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+                header.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+                header.setAcceptCharset(Collections.singletonList(StandardCharsets.UTF_8));
+            })
+            .bodyValue(getPayload(code))
+            .retrieve()
+            .bodyToMono(OAuthAccessToken.class)
+            .block();
     }
 
-    private MultiValueMap<String, String> getRequestHeaderForAuth() {
-        MultiValueMap<String, String> requestHeader = new LinkedMultiValueMap<>();
-        requestHeader.set("Accept", "application/json");
-        return requestHeader;
-    }
-
-    private MultiValueMap<String, String> getRequestPayloadForAuth(String code) {
-        MultiValueMap<String, String> requestPayload = new LinkedMultiValueMap<>();
-        requestPayload.set("client_id", System.getenv("CLIENT_ID"));
-        requestPayload.set("client_secret", System.getenv("CLIENT_SECRET"));
-        requestPayload.set("code", code);
-        return requestPayload;
+    private MultiValueMap<String, String> getPayload(String code) {
+        MultiValueMap<String, String> payload = new LinkedMultiValueMap<>();
+        payload.set("grant_type", "authorization_code");
+        payload.set("code", code);
+        return payload;
     }
 
     public AuthMemberInformation getAuthMemberInfo(OAuthAccessToken token) {
-        HttpEntity<?> request = new HttpEntity<>(getRequestHeaderForResource(token));
-        ResponseEntity<AuthMemberInformation> response = new RestTemplate().exchange(
-            GITHUB_RESOURCE_SERVER_USER_API_URL,
-            HttpMethod.GET,
-            request,
-            AuthMemberInformation.class
-        );
-
-        return response.getBody();
-    }
-
-    private MultiValueMap<String, String> getRequestHeaderForResource(OAuthAccessToken token) {
-        MultiValueMap<String, String> requestHeader = new LinkedMultiValueMap<>();
-        requestHeader.set("Accept", GITHUB_API_ACCEPT_HEADER);
-        requestHeader.set("Authorization", token.getAuthorizationValue());
-        return requestHeader;
+        return WebClient.create()
+            .get()
+            .uri(GITHUB_RESOURCE_SERVER_USER_API_URL)
+            .headers(header -> header.setBearerAuth(token.getAccessToken()))
+            .retrieve()
+            .bodyToMono(AuthMemberInformation.class)
+            .block();
     }
 }
