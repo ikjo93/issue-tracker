b1c3674455f048913320ae9d96efafb1dd785556
ikjo93 <auddlr100@naver.com>
2022-06-26 01:46:39 +0900

    feat : S3 이미지 업로드 및 레이블 CRUD 관련 예외 처리 핸들러 구현

--- be/src/main/java/codesquad/issuetracker/controller/ExceptionControllerAdvice.java
+++ be/src/main/java/codesquad/issuetracker/controller/ExceptionControllerAdvice.java
@@ -1,7 +1,9 @@
 package codesquad.issuetracker.controller;
 
 import codesquad.issuetracker.dto.ResponseMessage;
+import codesquad.issuetracker.exception.ImageUploadException;
 import codesquad.issuetracker.exception.InvalidTokenException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,5 +18,23 @@ public class ExceptionControllerAdvice {
     public ResponseEntity<ResponseMessage> handleJwtException(InvalidTokenException exception) {
         ResponseMessage message = new ResponseMessage(HttpStatus.UNAUTHORIZED, exception.getMessage());
         return new ResponseEntity<>(message, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<ResponseMessage> handleEmptyDataException() {
+        ResponseMessage message = new ResponseMessage(HttpStatus.NOT_FOUND, "존재하지 않는 정보입니다.");
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ResponseMessage> handleIllegalStateException(IllegalStateException exception) {
+        ResponseMessage message = new ResponseMessage(HttpStatus.NOT_FOUND, exception.getMessage());
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ImageUploadException.class)
+    public ResponseEntity<ResponseMessage> handleImageUploadException(ImageUploadException exception) {
+        ResponseMessage message = new ResponseMessage(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
