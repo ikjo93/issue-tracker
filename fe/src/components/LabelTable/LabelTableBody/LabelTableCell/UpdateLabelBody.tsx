f290b1913412cafbe9bd8a2bde492a743e0c1e8b
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-06-28 01:14:45 +0900

    feat: label patch

--- fe/src/components/LabelTable/LabelTableBody/LabelTableCell/UpdateLabelBody.tsx
+++ fe/src/components/LabelTable/LabelTableBody/LabelTableCell/UpdateLabelBody.tsx
@@ -1,8 +1,8 @@
 import AddIcon from '@mui/icons-material/Add';
 import CachedIcon from '@mui/icons-material/Cached';
+import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
 import axios from 'axios';
 import { FormEvent, useRef, useState } from 'react';
-import { useOutletContext } from 'react-router-dom';
 import styled, { useTheme } from 'styled-components';
 
 import Button from '@components/Button';
@@ -13,13 +13,8 @@ import Squircle from '@components/Squircle';
 import colors from '@constants/colors';
 import { fontSize } from '@constants/fonts';
 import { useLabelContext } from '@contexts/LabelProvider';
-import { OutletContext } from '@pages/LabelMilestoneLayout';
 import mixin from '@style/mixin';
 import { debounce } from '@util/timeUtils';
-
-const DEFAULT_LABEL_NAME = '레이블 이름';
-const POSSIBLE_COLOR_HEX_LEN = 4;
-const COLOR_HEX_MAX_LEN = 7;
 
 interface IFormEventTarget extends EventTarget {
   name?: HTMLInputElement;
@@ -28,25 +23,28 @@ interface IFormEventTarget extends EventTarget {
   darkText?: HTMLInputElement;
 }
 
-export default function CreateLabelBody() {
-  const [labelBgColor, setLabelBgColor] = useState('#EFF0F6');
-  const [isLabelDarkText, setIsLabelDarkText] = useState(true);
-  const [labelText, setLabelText] = useState(DEFAULT_LABEL_NAME);
+const DEFAULT_LABEL_NAME = '레이블 이름';
+const POSSIBLE_COLOR_HEX_LEN = 4;
+const COLOR_HEX_MAX_LEN = 7;
+
+export default function UpdateLabelBody({ label, toggleIsEditing }) {
+  const [labelBgColor, setLabelBgColor] = useState(label.color);
+  const [isLabelDarkText, setIsLabelDarkText] = useState(label.darkText);
+  const [labelText, setLabelText] = useState(label.name);
   const colorInputRef = useRef<HTMLInputElement>(null);
   const theme = useTheme();
-  const [, setIsAdding] = useOutletContext<OutletContext>();
   const { refetch: labelAxiosRefetch } = useLabelContext();
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     const formData: IFormEventTarget = e.target;
-    await axios.post('/api/labels/create', {
+    await axios.patch(`/api/labels/${label.id}`, {
       name: formData.name?.value,
       description: formData.description?.value,
       color: formData.color?.value,
       darkText: formData.darkText?.value,
     });
-    setIsAdding(false);
+    toggleIsEditing();
     labelAxiosRefetch();
   };
 
@@ -100,7 +98,7 @@ export default function CreateLabelBody() {
 
   return (
     <Wrapper onSubmit={handleSubmit}>
-      <Title>새로운 레이블 추가</Title>
+      <Title>레이블 편집</Title>
       <CreateInfo>
         <ExampleLabel>
           <Label
@@ -113,9 +111,14 @@ export default function CreateLabelBody() {
           <InputBox
             name="name"
             placeholder="레이블 이름"
+            defaultValue={label.name}
             onChange={handleChangeLabelNameInput}
           />
-          <InputBox name="description" placeholder="설명(선택)" />
+          <InputBox
+            name="description"
+            placeholder="설명(선택)"
+            defaultValue={label.description}
+          />
           <ColorInputs>
             <ColorInputBox width={15}>
               <InputCaption>배경 색상</InputCaption>
@@ -144,7 +147,7 @@ export default function CreateLabelBody() {
                   defaultValue="true"
                   name="darkText"
                   onChange={handleChangeRadioButton}
-                  defaultChecked
+                  defaultChecked={label.darkText}
                 />
                 <label htmlFor="selectDarkBtn">어두운 색</label>
                 <RadioButton
@@ -153,6 +156,7 @@ export default function CreateLabelBody() {
                   defaultValue="false"
                   name="darkText"
                   onChange={handleChangeRadioButton}
+                  defaultChecked={!label.darkText}
                 />
                 <label htmlFor="selectLightBtn">밝은 색</label>
               </Container>
@@ -161,6 +165,10 @@ export default function CreateLabelBody() {
         </InputBoxes>
       </CreateInfo>
       <ButtonBox>
+        <Button size="small" variant="outlined" onClick={toggleIsEditing}>
+          <CancelOutlinedIcon />
+          <span>취소</span>
+        </Button>
         <Button type="submit" size="small" variant="primary">
           <AddIcon />
           <span>완료</span>
@@ -220,6 +228,7 @@ const ButtonBox = styled.div`
   display: flex;
   justify-content: flex-end;
   width: 100%;
+  gap: 1rem;
 `;
 
 const CreateInfo = styled.div`
