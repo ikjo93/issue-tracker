993dc8f76091dbc4456c8f1575f55d8ec6f86555
ikjo93 <auddlr100@naver.com>
2022-06-28 05:14:03 +0900

    deploy : Issue-Tracker Version 1.01 (에러 개선)

--- be/src/main/java/codesquad/issuetracker/IssueTrackerApplication.java
+++ be/src/main/java/codesquad/issuetracker/IssueTrackerApplication.java
@@ -1,20 +1,14 @@
 package codesquad.issuetracker;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class IssueTrackerApplication {
 
-	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-		+ "classpath:application.yml,"
-		+ "classpath:aws.yml";
-
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(IssueTrackerApplication.class)
-			.properties(APPLICATION_LOCATIONS)
-			.run(args);
+		SpringApplication.run(IssueTrackerApplication.class, args);
 	}
 }
