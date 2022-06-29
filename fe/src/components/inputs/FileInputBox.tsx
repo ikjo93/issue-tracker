3774de8c6b3dd78318b5a0cb8cfe172d689da8e5
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-06-29 11:51:54 +0900

    feat: image upload form data

--- fe/src/components/inputs/FileInputBox.tsx
+++ fe/src/components/inputs/FileInputBox.tsx
@@ -1,24 +1,45 @@
 import AttachFileIcon from '@mui/icons-material/AttachFile';
+import { CircularProgress } from '@mui/material';
+import axios from 'axios';
 import { useRef, useState } from 'react';
 import styled from 'styled-components';
 
 import { fontSize } from '@constants/fonts';
 
-export default function FileInputBox() {
-  const [fileName, setFileName] = useState('');
+export default function FileInputBox({ textAreaRef }) {
+  const [isUploading, setIsUploading] = useState(false);
   const fileInputRef = useRef<HTMLInputElement>(null);
 
-  const handleChangeFileInput = () => {
+  const handleChangeFileInput = async () => {
     if (!fileInputRef.current) throw Error('file input ref is not found');
-    const newFileName = fileInputRef.current.value;
-    setFileName(newFileName);
+    const enableFileTypes = ['image/png', 'image/jpeg', 'image/gif'];
+    const enableMaxFileSize = 20_000_000; // 20MB
+    const { files } = fileInputRef.current;
+    if (!files) return;
+    // Only one file upload -> file is files[0]
+    if (!enableFileTypes.includes(files[0].type)) {
+      // Error Message
+    }
+    if (files[0].size > enableMaxFileSize) {
+      // Error Message
+    }
+    const formData = new FormData();
+    formData.append('images', files[0]);
+    setIsUploading(true);
+    const {
+      data: { imageLink },
+    } = await axios.post('http://3.38.208.189/api/images', formData);
+    const markDownString = `\n[${files[0].name}](${imageLink})`;
+    textAreaRef.current.value += markDownString;
+    setIsUploading(false);
   };
 
   return (
     <FileBox>
       <InputLabel htmlFor="input-file">
         <AttachFileIcon fontSize="inherit" />
-        {fileName === '' && <span>파일 첨부하기</span>}
+        <span>파일 첨부하기</span>
+        {isUploading && <CircularProgress color="inherit" size="0.6rem" />}
       </InputLabel>
       <input
         ref={fileInputRef}
@@ -26,13 +47,13 @@ export default function FileInputBox() {
         id="input-file"
         onChange={handleChangeFileInput}
       />
-      <UploadedFileName>{fileName}</UploadedFileName>
     </FileBox>
   );
 }
 
 const InputLabel = styled.label`
   display: flex;
+  gap: 0.5rem;
 `;
 
 const FileBox = styled.div`
@@ -40,5 +61,3 @@ const FileBox = styled.div`
   color: ${({ theme }) => theme.palette.placeholder};
   font-size: ${fontSize.xsmall};
 `;
-
-const UploadedFileName = styled.span``;
