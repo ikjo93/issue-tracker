4cb741cda242e9b18d273558610d66c16f621c63
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-06-30 02:45:07 +0900

    feat: markdown dark mode, light mode css import

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -10,7 +10,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
-      <GlobalStyle />
+      <GlobalStyle isDarkMode={isDarkMode} />
       <Routes />
     </ThemeProvider>
   );
